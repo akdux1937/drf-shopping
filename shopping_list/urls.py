dc9108083edd441dd0c7c93db90f58e67e4b0623
Andrew Krcatovich <andrew.krcatovich@gmail.com>
2023-07-21 16:18:53 -0500

    Tests for the ShoppingList and ShoppingItem

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -5,7 +5,7 @@ from shopping_list.api.views import AddShoppingItem, ListAddShoppingList, Shoppi
 
 urlpatterns = [
     path("api/shopping-lists/", ListAddShoppingList.as_view(), name='all-shopping-lists'),
-    path("api/shopping-lists/<uuid:pk>/", ShoppingListDetail.as_view(), name='shoopping-list-detail'),
+    path("api/shopping-lists/<uuid:pk>/", ShoppingListDetail.as_view(), name='shopping-list-detail'),
     path("api/shopping-lists/<uuid:pk>/shopping-items/", AddShoppingItem.as_view(), name='add-shopping-item'),
     path("api/shopping-lists/<uuid:pk>/shopping-items/<uuid:item_pk>/", ShoppingItemDetail.as_view(), name='shopping-item-detail'),
 ]
