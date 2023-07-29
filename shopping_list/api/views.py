353a843501b4aa1f7008684e24110a5311079a23
Andrew Krcatovich <andrew.krcatovich@gmail.com>
2023-07-29 10:09:43 -0500

    Ordering and pagination

--- shopping_list/api/views.py
+++ shopping_list/api/views.py
@@ -5,6 +5,7 @@ from shopping_list.api.permissions import (
     ShoppingItemShoppingListMembersOnly,
     ShoppingListMembersOnly,
 )
+from shopping_list.api.pagination import LargerResultsSetPagination
 from shopping_list.api.serializers import ShoppingItemSerializer, ShoppingListSerializer
 from shopping_list.models import ShoppingItem, ShoppingList
 
@@ -16,7 +17,7 @@ class ListAddShoppingList(generics.ListCreateAPIView):
         return serializer.save(members=[self.request.user])
 
     def get_queryset(self):
-        return ShoppingList.objects.filter(members=self.request.user)
+        return ShoppingList.objects.filter(members=self.request.user).order_by("-last_interaction")
 
 
 class ShoppingListDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -37,4 +38,16 @@ class ShoppingItemDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ShoppingItemSerializer
     permission_classes = [ShoppingItemShoppingListMembersOnly]
     lookup_url_kwarg = "item_pk"
+
+
+class ListAddShoppingItem(generics.ListCreateAPIView):
+    serializer_class = ShoppingItemSerializer
+    permission_classes = [AllShoppingItemsShoppingListMembersOnly]
+    pagination_class = LargerResultsSetPagination
+
+    def get_queryset(self):
+        shopping_list = self.kwargs["pk"]
+        queryset = ShoppingItem.objects.filter(shopping_list=shopping_list).order_by("purchased")
+
+        return queryset
 
--- shopping_list/apps.py
+++ shopping_list/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class ShoppingListConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shopping_list'
+
+
+class ApiConfig(AppConfig):
+    default = True
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "shopping_list"
+
+    def ready(self):
+        import shopping_list.receivers
+
